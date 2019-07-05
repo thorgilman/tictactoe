c60b7efdd67aa525ddf7b658ac4345878c196854
Nick Rogers <rogers.a.nick@gmail.com>
2019-07-05 14:51:40 -0400

    adding build files to static resource distribution

---
+++ clients/src/main/resources/static/precache-manifest.4cc2666d6d1ecf18f3d419da2a343a3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a49ad1e6ca3c8b4d385d68f6acdb519f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0239a3d648ad1aa936fb",
+    "url": "/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "0239a3d648ad1aa936fb",
+    "url": "/static/js/main.bd56ee22.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
