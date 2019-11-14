89d68303c2bb979019be40dc96b3fad8fcd0d413
thorgilman <thorgilman@gmail.com>
2019-11-14 16:32:00 -0500

    board_states db table creation working properly

--- contracts/src/main/kotlin/com/template/schemas/BoardSchema.kt
+++ contracts/src/main/kotlin/com/template/schemas/BoardSchema.kt
@@ -5,29 +5,36 @@ import net.corda.core.contracts.UniqueIdentifier
 import net.corda.core.identity.Party
 import net.corda.core.schemas.MappedSchema
 import net.corda.core.schemas.PersistentState
+import org.hibernate.annotations.Type
 import java.util.*
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.Table
 
-object BoardState
-object BoardStateSchemaV1 : MappedSchema(schemaFamily = BoardState.javaClass, version = 1, mappedTypes = listOf(PersistentBoardState::class.java)) {
+object BoardStateSchema
+object BoardStateSchemaV1 : MappedSchema(schemaFamily = BoardStateSchema.javaClass, version = 1, mappedTypes = listOf(PersistentBoardState::class.java)) {
     @Entity
-    @Table(name = "board_state")
+    @Table(name = "board_states")
     class PersistentBoardState(
             @Column(name = "playerO")
             var playerO: Party,
+
             @Column(name = "playerX")
             var playerX: Party,
+
             @Column(name = "isPlayerXTurn")
             var isPlayerXTurn: java.lang.Boolean,
+
             @Column(name = "board")
             var board: Array<CharArray>,
+
             @Column(name = "status")
             var status: Status,
+
             @Column(name = "linearId")
+            @Type(type = "uuid-char")
             var linearId: UUID
-    ) : PersistentState() {
-        //constructor(): this(Party(), Party(), false, Array(3, {charArrayOf('E', 'E', 'E')}, Status.GAME_IN_PROGRESS, UniqueIdentifier()))
-    }
+    ) : PersistentState()
+
+    override val migrationResource = "board_schema.changelog.init"
 }
